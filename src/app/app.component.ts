a7a5951611953d704be4d4cf84e5cf46848161ab
Kian Dadkhah <61067804+persianpapi-031@users.noreply.github.com>
2021-11-07 00:46:43 -0400

    name submission

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,19 @@
+/*********************************************************************************
+ *  WEB422 – Assignment 04
+ *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part of this
+ *  assignment has been copied manually or electronically from any other source (including web sites) or
+ *  distributed to other students.
+ *
+ *  Name: Kian Dadkhah Shokrollahi Student ID: 119369205 Date: 2021-11-07
+ *
+ ********************************************************************************/
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'web422-a';
